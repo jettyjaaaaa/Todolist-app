1102d72b5534a9a1e1298c952ff0d1ea24d83a5a
Wish Semangern <wishjet49@gmail.com>
2025-02-26 02:34:34 +0700

    config path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/todolistapp/", // Ensure this matches your Nginx path
+  base: "/todolistapp/", // Ensure this matches your Nginx path
   plugins: [react()],
   build: {
     outDir: "dist",
