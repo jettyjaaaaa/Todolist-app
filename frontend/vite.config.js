c4954766155fa7ac391afd658c42106f199df41c
Wish Semangern <wishjet49@gmail.com>
2025-02-26 17:00:54 +0700

    test-nginx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,13 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", 
     port: 5173, 
-    strictPort: true, 
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "https://jettyjaaaaa.space",
+        changeOrigin: true,
+        secure: true,
+      }
+    } 
   },
 });
