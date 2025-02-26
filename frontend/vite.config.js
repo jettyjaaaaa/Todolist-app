2b6e8863d597f6df0096763e5a8b9b2b130b1525
Wish Semangern <wishjet49@gmail.com>
2025-02-26 11:05:15 +0700

    config path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/todolistapp/", // Ensure this matches your Nginx path
+  base: "/todolistapp/", 
   plugins: [react()],
   build: {
     outDir: "dist",
     assetsDir: "assets",
   },
   server: {
-    port: 5173, // This is for local development, not used in Docker
+    host: "0.0.0.0", 
+    port: 5173, 
+    strictPort: true, 
   },
 });
