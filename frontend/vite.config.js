47a9aabc260c1f337fbf07d7d12f4b1a37741bd8
Wish Semangern <wishjet49@gmail.com>
2025-03-10 21:44:49 +0700

    fix api routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,22 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/todolistapp/", 
+  base: "/", 
   plugins: [react()],
   build: {
     outDir: "dist",
     assetsDir: "assets",
   },
   server: {
-    host: "0.0.0.0", 
-    port: 5173, 
+    host: "0.0.0.0",
+    port: 5173,
     strictPort: true,
     proxy: {
       "/api": {
         target: "https://todolist.jettyjaaaaa.space",
         changeOrigin: true,
-        secure: true,
-      }
-    } 
+        secure: false, 
+      },
+    },
   },
 });
