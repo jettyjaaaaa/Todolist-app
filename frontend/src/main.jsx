127246a63cac4d9a443514744f2d022c42e40116
Wish Semangern <wishjet49@gmail.com>
2025-03-12 00:05:32 +0700

    fix frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../src/assets/styles.css";
-// import "../src/assets/modal.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
